72c411ad21d0aac30456d3c3e40c070c89ab62a7
Justin Zaun <justin.zaun@gmail.com>
2026-03-13 14:51:34 -1000

    Bump version to 0.1.3

--- compiler/include/version.h
+++ compiler/include/version.h
@@ -12,8 +12,8 @@
 
 #define JZ_HDL_VERSION_MAJOR 0
 #define JZ_HDL_VERSION_MINOR 1
-#define JZ_HDL_VERSION_PATCH 2
+#define JZ_HDL_VERSION_PATCH 3
 
-#define JZ_HDL_VERSION_STRING "Version 0.1.2 (unknown)"
+#define JZ_HDL_VERSION_STRING "Version 0.1.3 (unknown)"
 
 #endif /* JZ_HDL_VERSION_H */
